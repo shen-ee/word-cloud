aff7261a5def4d42b9fd033ca48b796b5fa39c9d
Shen Yi <jerry31@qq.com>
2018-11-13 00:40:19 -0800

    Update

--- tokenization.py
+++ tokenization.py
@@ -1,21 +1,29 @@
 # coding:utf-8
 import thulac	
-input = "raw.txt"
-output = "output.txt"
+# 
+# 
 
-def read_text(filename):
+def read_txt(input_filename):
     str = ""
-    with open(filename,'r') as raw_text:
+    with open(input_filename,'r') as raw_text:
         for line in raw_text:
             # print(line)
             str+=line
     return str
 
-thu1 = thulac.thulac(seg_only=True)  #默认模式
-str = read_text(input)
-text = thu1.cut(str, text=True)  #进行一句话分词
+def tokenize(input_filename):
+    thu1 = thulac.thulac(seg_only=True)  #默认模式
+    str = read_txt(input_filename)
+    text = thu1.cut(str, text=True)  #进行一句话分词
 
-with open(output,'w') as output_txt:
-    output_txt.write(text)
+    # print(text)
+    return text
 
-print(text)
+
+if __name__ == "__main__":
+    input = "raw.txt"
+    output = "output.txt"
+    text = tokenize(input)
+    with open(output,'w') as output_txt:
+        output_txt.write(text)
+    print(text)
